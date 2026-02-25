41fa114744e5ee71f6d36fad0d0621460013fcf5
ouslimaneabdelilah <abdelilah.ouslimane@gmail.com>
2026-02-25 22:15:00 +0000

    feat: add StoreCategoryRequest for validating category creation requests

---
+++ app/Http/Requests/Categories/StoreCategoryRequest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Requests\Categories;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreCategoryRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    public function rules(): array
+    {
+        return [
+            'name' => ['required', 'string', 'max:255']
+        ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            'name.required' => 'Le nom de la catégorie est requis.',
+            'name.string' => 'Le nom doit être une chaîne de caractères.',
+            'name.max' => 'Le nom ne peut pas dépasser 255 caractères.'
+        ];
+    }
+}
