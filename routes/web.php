3c8302b2f3e22a416043816ad6c75fbebdaf127c
ouslimaneabdelilah <abdelilah.ouslimane@gmail.com>
2026-02-25 23:30:00 +0000

    feat: add route for expense index

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\AuthController;
 use App\Http\Controllers\ColocationController;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\ExpenseController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -23,4 +24,6 @@ Route::middleware('auth')->group(function () {
     Route::resource('colocations', ColocationController::class);
     Route::post('/colocations/{colocation}/cancel', [ColocationController::class, 'cancel'])->name('colocations.cancel');
     Route::resource('colocations.categories', CategoryController::class)->except(['create', 'show']);
+    Route::get('/colocations/{colocation}/expenses', [ExpenseController::class, 'index'])->name('expenses.index');
+
 });
