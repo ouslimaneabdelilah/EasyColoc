81c53ece81316f8248077ade99cc43076fe82fe6
ouslimaneabdelilah <abdelilah.ouslimane@gmail.com>
2026-02-26 11:00:00 +0000

    feat: add database seeders and fix user relationships

---
+++ app/Http/Controllers/DashboardController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class DashboardController extends Controller
+{
+    public function index()
+    {
+        $user = Auth::user();
+
+        if ($user->role === 'admin') {
+            return redirect()->route('admin.stats');
+        }
+
+        return redirect()->route('colocations.index');
+    }
+}
